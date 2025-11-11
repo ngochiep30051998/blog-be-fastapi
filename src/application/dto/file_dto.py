31b86cbe70b5b34aa81ee23222b899097f719027
hiep.nguyen <hiep.nguyen@itech.asia>
2025-11-11 13:15:46 +0700

    validate_by_name

--- src/application/dto/file_dto.py
+++ src/application/dto/file_dto.py
@@ -20,7 +20,7 @@ class FileResponse(BaseModel):
     uploaded_by: Optional[PyObjectId] = None
 
     class Config:
-        allow_population_by_field_name = True
+        validate_by_name = True
         arbitrary_types_allowed = True
         json_encoders = {PyObjectId: str}
     
